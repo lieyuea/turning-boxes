18396d24574ecda2e1e1d3aa80a7e00c2ebd1b70
lieyuea <mjoshuahua@gmail.com>
2023-03-02 23:21:36 -0800

    Reposition tick method

--- app/main.rb
+++ app/main.rb
@@ -1,28 +1,4 @@
-$gtk.reset
-
 require 'app/box.rb'
 require 'app/button.rb'
 require 'app/game.rb'
-require 'app/menu.rb'
-
-def tick args
-  args.state.gamestate ||= Game.new args
-  args.state.gamestate.args = args
-  args.state.gamestate.tick
-
-  args.outputs.debug << [
-    {
-      x: 0,
-      y: 720,
-      text: "mouse coord: #{args.inputs.mouse.x}, #{args.inputs.mouse.y}"
-    }, {
-      x: 0,
-      y: 700,
-      text: "framerate: #{args.gtk.current_framerate}"
-    }, {
-      x: 0,
-      y: 680,
-      text: "state: #{args.state.gamestate.class}"
-    }
-  ]
-end
+require 'app/tick.rb'
---
+++ app/tick.rb
@@ -0,0 +1,22 @@
+$game = Game.new $args
+
+def tick args
+  $game.args = args
+  $game.tick
+
+  args.outputs.debug << [
+    {
+      x: 0,
+      y: 720,
+      text: "mouse coord: #{args.inputs.mouse.x}, #{args.inputs.mouse.y}"
+    }, {
+      x: 0,
+      y: 700,
+      text: "framerate: #{args.gtk.current_framerate}"
+    }, {
+      x: 0,
+      y: 680,
+      text: "state: #{args.state.gamestate.class}"
+    }
+  ]
+end
